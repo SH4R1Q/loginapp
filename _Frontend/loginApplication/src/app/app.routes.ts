f115de0abc1091caff9672a1fe4877bd905164fb
Shariq Khan <94235303+SH4R1Q@users.noreply.github.com>
2023-12-03 20:17:20 +0530

    basic application is ready

--- _Frontend/loginApplication/src/app/app.routes.ts
+++ _Frontend/loginApplication/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:LoginPageComponent,
+        pathMatch:'full'
+    },
+    {
+        path:"home",
+        component:HomeComponent,
+        pathMatch:'full'
+    }
+];
--- _Frontend/loginApplication/src/app/components/login-page/login-page.component.ts
+++ _Frontend/loginApplication/src/app/components/login-page/login-page.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BackendService } from '../../Services/backend.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [FormsModule,HttpClientModule],
+  imports: [FormsModule,HttpClientModule,CommonModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
@@ -16,14 +16,21 @@ export class LoginPageComponent {
     userName:"",
     password:""
   }
+  flag:boolean = false
   private baseUrl: String = "http://localhost:8080"
 
   constructor(private http: HttpClient){}
   btnClick(){
     (this.data.userName.length===0 || this.data.password.length===0)?console.log("error"):
     this.http.post(`${this.baseUrl}/login`, this.data).subscribe(
-      response=>{
-        console.log(response);
+      (response:any)=>{
+        if(!(response.password===null)){
+          window.location.href="/home"
+          this.flag = false;
+        }
+        else{
+          this.flag = true;
+        }
       },
       error=>{
         console.log(error);
